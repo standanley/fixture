220ddbf95df2ff8d0301e7de43236ce0e35403bd
Daniel Stanley <dstanley@Stanford.edu>
2019-09-19 10:46:15 -0700

    linearregression

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="facet",
+    name="fixture",
     version="0.0.1",
     author="Daniel Stanley",
     author_email="dstanley@Stanford.edu",
     description="Tools for creating digital models of analog things",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/standanley/facet",
+    url="https://github.com/standanley/fixture",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
