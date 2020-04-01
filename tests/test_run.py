e8e77903d0cd6a4c52dacd5015639ef24a0b98f5
Daniel Stanley <dstanley@Stanford.edu>
2020-04-01 16:28:53 -0700

    Remove redundant test, fix bugs in tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,9 +5,7 @@ def file_relative_to_test(fname):
 
 def test_simple_amp():
     circuit_fname = file_relative_to_test('./configs/simple_amp.yaml')
-    test_fname = file_relative_to_test('./configs/ngspice.yaml')
-
-    command = 'python -m fixture.run %s %s' % (circuit_fname, test_fname)
+    command = 'python -m fixture.run %s' % (circuit_fname)
     print('about to run!')
     res = subprocess.run(command.split(), check=True)
     print('<<<')
@@ -23,25 +21,22 @@ def test_simple_amp_direct():
 
 def test_parameterized_amp():
     circuit_fname = file_relative_to_test('./configs/parameterized_amp.yaml')
-    test_fname = file_relative_to_test('./configs/ngspice.yaml')
-    command = 'python -m fixture.run %s %s' % (circuit_fname, test_fname)
+    command = 'python -m fixture.run %s' % (circuit_fname)
     res = subprocess.run(command.split(), check=True)
 
 def test_differential_amp():
     circuit_fname = file_relative_to_test('./configs/differential_amp.yaml')
-    test_fname = file_relative_to_test('./configs/ngspice.yaml')
-    command = 'python -m fixture.run %s %s' % (circuit_fname, test_fname)
+    command = 'python -m fixture.run %s' % (circuit_fname)
     res = subprocess.run(command.split(), check=True)
 
 def test_differential_amp_B():
     circuit_fname = file_relative_to_test('./configs/differential_amp_B.yaml')
-    test_fname = file_relative_to_test('./configs/ngspice.yaml')
-    command = 'python -m fixture.run %s %s' % (circuit_fname, test_fname)
+    command = 'python -m fixture.run %s' % (circuit_fname)
     res = subprocess.run(command.split(), check=True)
 
 if __name__ == '__main__':
     #test_simple_amp()
-    #test_parameterized_amp()
+    test_parameterized_amp()
     #test_simple_amp_direct()
-    test_differential_amp()
-    test_differential_amp_B()
+    #test_differential_amp()
+    #test_differential_amp_B()
