87aa7f131be148b83b1eadd19df841487863d882
Daniel Stanley <dstanley@Stanford.edu>
2020-10-26 14:26:47 -0700

    Started config parse, breaks all spice buses

--- fixture/create_testbench2.py
+++ fixture/create_testbench2.py
@@ -1,5 +1,6 @@
 import fixture
 import re
+import yaml
 
 class SignalIn():
     def __init__(self,
@@ -93,6 +94,7 @@ class EmptyTemplate():
         'myname<5> -> (True, 'myname', 5>
         'myname<5><10> -> (True, 'myname<5>', 10)
         '''
+        assert False, 'use break_bus_name instead?'
         if name == None:
             return (False, None, None)
 
@@ -257,15 +259,104 @@ class AmpTemplate(EmptyTemplate):
 def run(template, test_info):
     assert isinstance(template, EmptyTemplate)
 
+    for test_name in test_info:
+        test = getattr(template, test_name)()
+        assert isinstance(test, EmptyTemplate.Test)
+
+def break_bus_name(bus_name):
+    '''
+    'myname' -> (False, None, None)
+    'myname<5> -> (True, 'myname', 5>
+    'myname<5><10> -> (True, 'myname<5>', 10)
+    '''
+    #if name == None:
+    #    return (False, None, None)
+
+    def regex_escape(c):
+        if c in '[]\\^()|':
+            return '\\' + c
+        else:
+            return c
+
+    # NOTE these delims are searched for with regex, so [] need escaping
+    delims = ['<:>', '[:]']
+    for delim in delims:
+        open, mid, close = regex_escape(delim[0]), regex_escape(delim[1]), regex_escape(delim[2])
+        match = re.match(f'^(.*?){open}([0-9])+:([0-9]+){close}(.*)$', bus_name)
+        if match:
+            bus_name = match.group(1)
+            start = int(match.group(2))
+            end = int(match.group(3))
+            post = match.group(4)
+            direction = 1 if end >= start else -1
+            bus_name_sub = bus_name + delim[0] + '%d' + delim[2] + post
+            indices = range(start, end+direction, direction)
+            return (bus_name_sub, indices)
+    # TODO should I wrap the bus_name in a list?
+    return bus_name
+
+def parse_cfg(circuit_config_filename):
+    with open(circuit_config_filename) as f:
+        #d = yaml.safe_load(f)
+        #d['filename'] = circuit_config_filename
+
+        #assert 'template' in d
+        #assert 'spice_name' in d
+        #assert 'spice_io' in d
+
+        d = {
+            'spice_io': {
+                'bus1<2:0>[0:1]': {'dtype': 1, 'template_name': 'req'},
+                'sampler_clk_only': {'template_name': 'clk[0]'},
+                'pb<0>': {'template_name': 'pb_a'}
+            }
+        }
+
+        # edit spice pins to break buses into their individual components
+        def break_bus(pin_name, pin_dict):
+            '''
+            break_bus('mybus<1:0>', {abc: def, 'template_name': 'req[3]')
+            ->
+            {'mybus<1>: {abc: def, 'template_name': 'req[3][1]'},
+             'mybus<0>: {abc: def, 'template_name': 'req[3][0]'}}
+            '''
+            name = break_bus_name(pin_name)
+            if type(name) is str:
+                return {pin_name: pin_dict}
+            else:
+                bus_name_sub, indices = name
+                ans = {}
+                for i in indices:
+                    pin_name = bus_name_sub % i
+                    pin_dict_copy = pin_dict.copy()
+                    if 'template_name' in pin_dict_copy:
+                        pin_dict_copy['template_name'] += f'[{i}]'
+                    ans.update(break_bus(pin_name, pin_dict_copy))
+
+                return ans
+
+        spice_io = d['spice_io']
+        new_spice_io = {}
+        for pin_name in spice_io.keys():
+            # TODO extract bus delim?
+            new_spice_io.update(break_bus(pin_name, spice_io[pin_name]))
+        print(new_spice_io)
+
 
 
 
 if __name__ == '__main__':
 
-    print(EmptyTemplate.check_name_for_bus(None, 'testing<02>'))
-    print(EmptyTemplate.check_name_for_bus(None, 'testing<02>[50]'))
-    print(EmptyTemplate.check_name_for_bus(None, 'testing<0g>'))
-    print(EmptyTemplate.check_name_for_bus(None, 'testing<0g>hi'))
+    #print(EmptyTemplate.check_name_for_bus(None, 'testing<02>'))
+    #print(EmptyTemplate.check_name_for_bus(None, 'testing<02>[50]'))
+    #print(EmptyTemplate.check_name_for_bus(None, 'testing<0g>'))
+    #print(EmptyTemplate.check_name_for_bus(None, 'testing<0g>hi'))
+    print(break_bus_name('testing<02:5>'))
+    print(break_bus_name('testing<2:0>[5:0]'))
+    print(break_bus_name('testing<0:g>'))
+    print(break_bus_name('testing<0:5>hi'))
+
+    parse_cfg('fixture/example_config.yaml')
 
     template_name = AmpTemplate
 
