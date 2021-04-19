3f7ac8ea8da10897527c14a06beea22a29036007
Daniel Stanley <dstanley@Stanford.edu>
2021-04-19 08:48:41 -1000

    Incremental update (#21)
    * Update internal port mapping to store bus elements individually
    
    * Automatically break buses from input cfg
    
    * Add mac and dac templates.
    
    * Start conversion to using signals. Change bus detection in regression to use port mapping.
    
    * Small fix in simple_amp
    
    * Small updates to improve pytest performance. Now just missing mac spice model
    
    * First pass at generated mac, not working yet
    
    * Working mac template
    
    * Various incremental updates that have been building up. Most significant is post-regression phase.
    regression: save model results
    template_master: post-regression phase
    differential_amp_B: an overhaul to use it for testing differential strategies.
    mac: add post-regression phase
    sampler: minimal changes
    phase_blender: minimal changes
    sampler_custom: minimal changes
    mac_gen: added random noise capability
    generated_mac: use new mac_gen
    test_run: add test for new differential_amp2_b
    
    * Initial sky130 additions. Have not yet added sky130 installation to regression
    
    * mgenero updates to work with new signal interface
    
    * Minor updates to gen_pb
    
    * Add required_info to templates
    
    * Add fixture/config.py to automatically generate config file with todo markers.
    
    * Add regression test for config gen
    
    * Add some more skywater versions
    
    * Working on new phase blender model
    
    * Testing phase blender C with 16 bit thermometer input
    
    * Phase blender tests should pass now although they are slow. Added some older exploration code in run.py which I should remove
    
    * Either removed old things or put them in 'investigation' folder, which should probably not be committed to master branch
    
    * Started replacing some use of template.input_ba with use of template.signals
    
    * Updated phase blender model to use Signals, but other tests are still broken
    
    * Working on more tests using signals
    
    * Got phase_blender_C template working with signals mostly. No support yet for certain buses in config and no support yet for required ba.
    
    * Buses now work as far as the config parsing is concerned. Still an issue with providing random samples to the testbench() function with buses involved.
    
    * mac now passes
    
    * Updated all configs to new style
    
    * DAC now works. Also moved SignalManager to the signal file.
    
    * Minor fixes
    
    * Fixed mgenero interface to use signals
    
    * Fixed test_regression and test_sampler4
    
    * Fixed some simple_amp issues and added a test file for differential amps
    
    * Got first sampler model working by making sure unused clock was driven"
    
    * Fix oscillator
    
    * Move misplaced type check, update configs
    
    * Updatedsome tests, everything now passes on my end! May need to make some changes to make it pass on Travis as well
    
    * Add pip freeze debug stuff
    
    * Try updating python to 3.8
    
    * Last commit seemed to do the trick. Now removing broken debug statemetn
    
    * Switching back to 3.7 to confirm that is actually the issue, because we are running 3.7.5 locally and the code is not ready for 3.8
    
    * Trying python 3.7.5
    
    * Undo change in install_fault
    
    * Remove hardcoded filename in congfig_gen, turn off plot in mac. I believe the last thing to do before Travis passes is figure out Sky130 installation.
    
    * Added pytest skip if Sky130 is not installed. In the future I'd like to get it installed with Travis.
    
    * Missed one Sky130 test on the last commit
    
    * Partway through removing old unused stuff.
    
    * Updates to sampler template I missed before to use signals.
    
    * Massively cut unused stuff from TemplateMaster
    
    * Create interactive config gen.
    
    * New js config gen now works. Still very clumsy
    
    * Fix small issue with test ngspice

--- fixture/config.py
+++ fixture/config.py
@@ -1,13 +1,166 @@
 import fixture
 import fault
 import sys
+import html
 
 header = '''
 # Generated configuration file
 # Please fill in anything marked <TODO>
 
 '''
+
+html_header = '''
+<head> <meta charset="UTF-8"> </head>
+<h1> This is the fixture interactive config generator</h1>
+<h2> Just fill out the form and then copy the config file at the bottom </h2>
+
+'''
 TAB = '    '
+
+def make_config_interactive(spice_filename, template_name, skip_writing_file=False):
+    form = []
+    config = []
+    js = []
+    js_startup = []
+
+    def form_entry(id):
+        return f'<span id="{id}"></span>'
+
+    def radio(id, title, options):
+        ''' takes care of form and js'''
+        form_id = id + '_radio'
+        form.append('\n')
+        form.append(f'<label for="{form_id}">{title}</label>')
+        for option in options:
+            form.append(f'<input type="radio" name="{form_id}" value={option} onchange="{id}_fun()"> {option} </input>')
+        form.append('\n<br>')
+
+        js.append('\n')
+        js.append(f'function {id}_fun() {{')
+        js.append(f'{TAB}buttons = document.getElementsByName("{form_id}");')
+        js.append(f'{TAB}value="TODO";')
+        js.append(f'{TAB}buttons.forEach( button => {{')
+        js.append(f'{TAB}{TAB}if(button.checked) {{ value = button.value; }}')
+        js.append(f'{TAB}}});')
+        js.append(f'{TAB}document.getElementById("{id}").innerHTML = value;')
+        js.append(f'}}')
+
+        js_startup.append(f'{id}_fun();')
+
+    def dropdown(id, title, options):
+        ''' takes care of form and js '''
+        form_id = id + '_select'
+        form_name = id + '_select_name'
+        form.append('')
+        form.append(f'<label for="{form_id}">{title}</label>')
+        form.append(f'<select id="{form_id}" name="{form_name}" onchange="{id}_fun()">')
+        for option in options:
+            form.append(f'  <option value="{option}"> {option} </option>')
+        form.append(f'</select> <br>')
+
+        js.append('')
+        js.append(f'function {id}_fun() {{')
+        js.append(f'{TAB}value = document.getElementById("{form_id}").value;')
+        js.append(f'{TAB}document.getElementById("{id}").innerHTML= value;')
+        js.append(f'}}')
+
+        js_startup.append(f'{id}_fun();')
+
+    def text(id, title, optional_key=None):
+        ''' takes care of form and js '''
+        form_id = id + '_text'
+        form.append('')
+        form.append(f'<label for="{form_id}">{title}</label>')
+        form.append(f'<input type="text" id="{form_id}" oninput="{id}_fun()"/> <br>')
+
+        js.append('')
+        js.append(f'function {id}_fun() {{')
+        js.append(f'{TAB}value = document.getElementById("{form_id}").value;')
+        js.append(f'{TAB}form_span = document.getElementById("{id}");')
+        if optional_key is None:
+            js.append(f'{TAB}form_span.innerHTML = value;')
+        else:
+            js.append(f'{TAB}if (value=="") {{form_span.innerHTML = value;}}')
+            # NOTE this always inserts an extra blank line ... which is okay for my usage so far
+            js.append(f'{TAB}else {{form_span.innerHTML = "        {optional_key}: "+value+"\\n";}}')
+        js.append(f'}}')
+
+        js_startup.append(f'{id}_fun();')
+
+
+
+    template = getattr(fixture.templates, template_name)
+    circuit = fault.spice_target.DeclareFromSpice(spice_filename)
+
+    circuit_name = circuit.name
+    ports = circuit.IO.ports
+
+    # circuit name
+    config.append(f'circuit_name: {circuit_name}')
+
+    # pins
+    config.append('\n# pins')
+    for pin_name in ports.keys():
+
+        pin_name_clean = html.escape(pin_name).replace('&', '_').replace(';', '_')
+
+        form.append('<br>')
+        config.append(f'{TAB}{pin_name}:')
+
+        direction_id = pin_name_clean + '_direction'
+        config.append(f'{TAB}{TAB}direction: {form_entry(direction_id)}')
+        radio(direction_id, f'"{pin_name}" Direction:', ['input', 'output'])
+
+        type_id = pin_name_clean + '_type'
+        config.append(f'{TAB}{TAB}datatype: {form_entry(type_id)}')
+        type_options = ['TODO', 'bit', 'real', 'analog', 'binary_analog', 'true_digital']
+        dropdown(type_id, f'"{pin_name}" Data Type:', type_options)
+
+        value_id = pin_name_clean + '_value'
+        config.append(form_entry(value_id))
+        text(value_id, f'"{pin_name}" Value (float for pinned value, pair of floats for range, or blank): ', 'value')
+
+    # mapping
+    pin_choices = ['TODO', *ports.keys()]
+    form.append('<br>')
+    config.append('')
+    config.append('# Mapping between template and user pins')
+    config.append('template_pins:')
+    for required in template.required_ports:
+        required_clean = html.escape(required).replace('&', '_').replace(';', '_')
+        map_name = required_clean + '_map'
+
+        config.append(f'{TAB}{required}: {form_entry(map_name)}')
+        dropdown(map_name, f'Circuit port that corresponds to required port "{required}": ', pin_choices)
+
+
+    # extras
+    form.append('<br>')
+    config.append('')
+    config.append('# Additional template-required information')
+    config.append('extras:')
+    for k, v in template.required_info.items():
+        extra_id = k + '_extra'
+        config.append(f'{TAB}{html.escape(k)}: {form_entry(extra_id)}')
+        text(extra_id, v)
+
+    temp = '''
+function test() {
+        var newWindow = window.open("name.yaml", "_blank");
+        debugger;
+        newWindow.document.write("test:\\n    attr: yep\\n");
+    }
+
+    test();
+'''
+
+    form_text = '<form>\n    ' + '\n    '.join(form) + '\n</form>'
+    config_text = '<hr><br>\n<pre>\n' + '\n'.join(config) + '\n</pre>'
+    js_text = '<script type="text/javascript">\n    ' + '\n    '.join(js+js_startup)+temp + '\n</script>'
+
+    everything = '\n\n'.join([html_header, form_text, config_text, js_text])
+    return everything
+
 
 def make_config(spice_filename, template_name, skip_writing_file=False):
     template = getattr(fixture.templates, template_name)
@@ -25,21 +178,24 @@ def make_config(spice_filename, template_name, skip_writing_file=False):
     lines.append('')
 
     # pins
-    if len(template.required_ports) > 0:
-        lines.append('# TODO Tag pins with each of the following required inputs/outputs:')
-        for name in template.required_ports:
-            lines.append(f'#{TAB*2}template_pin: {name}')
-
     lines.append('pins:')
     for pin_name in ports.keys():
         lines.append(f'{TAB}{pin_name}:')
         lines.append(f'{TAB*2}direction: <TODO "input" or "output">')
         lines.append(f'{TAB*2}datatype: <TODO "real", "digital", or "binary_analog">')
-        lines.append(f'{TAB*2}<TODO possibly add "value" or "template_pin" and/or "width">')
 
+    # mapping
+    if len(template.required_ports) > 0:
+        lines.append('# TODO Tag each of the following required inputs/outputs with a pin')
+        lines.append('template_pins:')
+        for name in template.required_ports:
+            lines.append(f'#{TAB}{name}: <TODO>')
+
+    # extras
     lines.append('')
+    lines.append('extras:')
     for k, v in template.required_info.items():
-        lines.append(f'{k}: <TODO {v}>')
+        lines.append(f'{TAB}{k}: <TODO {v}>')
 
     lines.append('')
     lines.append('test_config_file: <TODO>')
@@ -58,5 +214,5 @@ if __name__ == '__main__':
         print('Must specify spice_filename and template_name as command line arguments')
     spice_filename = args[1]
     template_name = args[2]
-    make_config(spice_filename, template_name)
+    make_config_interactive(spice_filename, template_name)
 
