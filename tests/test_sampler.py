95d30e2346895967001de96903fe213cf3b1d1fd
Daniel Stanley <dstanley@Stanford.edu>
2021-07-27 11:41:23 -0700

    Random thermometer fix (#23)
    * Changed digital sampling to be more friendly to thermometer coded stuff
    
    * Updated requirements to specify numpy version
    
    * Fixed digital 1/0 balance in sampler

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -25,18 +25,20 @@ def test_analog():
     for test in range(100):
         d = random.randrange(1, 10)
         N = random.randrange(d+1, 300)
-        print(f'Testing {d} dimensions, {N} samples')
+        print(f'Testing {d} analog dimensions, {N} samples')
 
         s = Sampler.get_orthogonal_samples(d, 0, N)
+        assert len(s) == N
         Sampler.assert_lhs(s)
 
 def test_digital():
     for test in range(100):
         d = random.randrange(1, 25)
         N = random.randrange(d+1, 300)
-        print(f'Testing {d} dimensions, {N} samples')
+        print(f'Testing {d} digital dimensions, {N} samples')
 
         s = Sampler.get_orthogonal_samples(0, d, N)
+        assert len(s) == N
         Sampler.assert_fifty_fifty(s)
 
 def test_combined():
@@ -47,6 +49,7 @@ def test_combined():
         print(f'Testing {Da} analog dimensions, {Dd} digital, {N} samples')
 
         s = Sampler.get_orthogonal_samples(Da, Dd, N)
+        assert len(s) == N
 
         reorg = list(zip(*s))
         analog_samples = list(zip(*(reorg[:Da])))
@@ -55,3 +58,8 @@ def test_combined():
         Sampler.assert_lhs(analog_samples)
         Sampler.assert_fifty_fifty(digital_samples)
 
+if __name__ == '__main__':
+    test_assert_fifty_fifty()
+    test_digital()
+    test_analog()
+    test_combined()
