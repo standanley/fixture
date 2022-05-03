036a5527f1b6b1f3ee6b024e61819470abfe088d
Daniel Stanley <dstanley@Stanford.edu>
2022-05-03 15:54:16 -0700

    Accidentally had debugging turned on in previous commit

--- fixture/template_master.py
+++ fixture/template_master.py
@@ -6,7 +6,7 @@ from fixture.signals import SignalManager, SignalArray, SignalOut
 from fixture.plot_helper import PlotHelper
 
 class TemplateMaster():
-    debug = True
+    debug = False
 
     class Ports:
         def __init__(self, signal_manager):
