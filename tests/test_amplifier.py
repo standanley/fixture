8c6df4a911777f507305e72f42dcdf8cc760ba46
Daniel Stanley <dstanley@Stanford.edu>
2023-02-16 11:56:59 -0800

    Many changes to tests folder. Some new tests and some inconsequential edits

--- tests/test_amplifier.py
+++ tests/test_amplifier.py
@@ -19,6 +19,7 @@ tests = [
     ('configs/tia2.yaml', None),
     ('configs/tia3.yaml', None),
     ('configs/tia4.yaml', None),
+    ('configs/tia3_amp.yaml', None),
 
 ]
 
@@ -33,5 +34,5 @@ def test_by_config(test):
     fixture.run(circuit_fname)
 
 if __name__ == '__main__':
-    test_by_config(tests[7])
+    test_by_config(tests[0])
 
--- tests/test_phase_blender.py
+++ tests/test_phase_blender.py
@@ -56,8 +56,8 @@ def test_therm16_model():
 
 if __name__ == '__main__':
     #test_generated()
-    test_ngspice()
+    #test_ngspice()
     #test_spectre()
     #test_C_sky130()
     #test_C_sky130_therm16()
-    #test_therm16_model()
+    test_therm16_model()
